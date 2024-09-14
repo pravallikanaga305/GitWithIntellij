0034da88fdf809c8ce8112622cdc7c96c88f0d2f
mnpravallika <mnpravallika173@gmail.com>
2024-09-14 14:41:57 -0500

    First Commit

---
+++ src/main/java/org/example/Dev.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Dev {
+
+    public static void main(String[] args) {
+        System.out.println(" hello this is from Dev Class");
+    }
+}
---
+++ src/test/java/DevTest.java
@@ -0,0 +1,6 @@
+public class DevTest {
+
+    public static void main(String[] args) {
+        System.out.println(" hello this is from DevTest Class");
+    }
+}
